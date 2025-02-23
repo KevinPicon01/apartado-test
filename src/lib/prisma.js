a8f894a9a412608a444d6a1e203b87cb66a744a8
digital <kgpicon@hotmail.com>
2025-02-22 20:38:07 -0500

    install

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis;
-
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
